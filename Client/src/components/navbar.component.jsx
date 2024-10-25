e4819f3c7c155205e9ba3d810433ac14f17bae4c
Santosh Khadka <santoshkhadka2326@gmail.com>
2024-10-25 13:08:32 +0545

    usersign out is implemented

--- Client/src/components/navbar.component.jsx
+++ Client/src/components/navbar.component.jsx
@@ -1,8 +1,16 @@
 import logo from "../imgs/logo.png";
 import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { useSession } from "../context/User.context";
+import UserNavigationPanel from "./user-navigation.component";
 const Navbar = () => {
   const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
+
+  const userState = useSession();
+  const {
+    userAuth,
+    userAuth: { profile_img, accessToken },
+  } = userState;
 
   return (
     <>
@@ -42,12 +50,35 @@ const Navbar = () => {
             <p>Write</p>
           </Link>
 
-          <Link className="btn-dark py-2" to="/signin">
-            Sign In
-          </Link>
-          <Link className="btn-light py-2 hidden md:block" to="/signup">
-            Sign Up
-          </Link>
+          {accessToken ? (
+            <>
+              <Link to="/dashboard/notification">
+                <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
+                  <i className="fi fi-rr-bell text-2xl block mt-1"></i>
+                </button>
+              </Link>
+
+              <div className="relative ">
+                <button className="w-12 h-12 mt-1">
+                  <img
+                    src={profile_img}
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </button>
+
+                <UserNavigationPanel />
+              </div>
+            </>
+          ) : (
+            <>
+              <Link className="btn-dark py-2" to="/signin">
+                Sign In
+              </Link>
+              <Link className="btn-light py-2 hidden md:block" to="/signup">
+                Sign Up
+              </Link>
+            </>
+          )}
         </div>
       </nav>
 
