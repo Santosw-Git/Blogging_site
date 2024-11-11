b45f5888fe61d4fa190bbc70634aa681995c145f
Santosh Khadka <santoshkhadka2326@gmail.com>
2024-11-11 17:45:27 +0545

    file is uploaed sucessfully

--- server/controllers/file.controller.js
+++ server/controllers/file.controller.js
@@ -5,20 +5,16 @@ import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 
 const FileUpload = asyncHandler (async (req,res)=>{
 
-    console.log("Uploading")
     const file = req.file?.path;
-    console.log(file);
-    
-
+    // console.log(file);
     const result = await uploadOnCloudinary(file);
-    console.log("result: " + result);
-    
-    
-    
-    
 
+    if(!result){
+        return res.status(400).json({"error": "error uploading on cloudinary"});
+    }
 
-
+    return res.status(200).json({"url": result.url});
+    
 
 });
 
--- server/utils/Cloudinary.js
+++ server/utils/Cloudinary.js
@@ -12,33 +12,24 @@ cloudinary.config({
 
 
 const uploadOnCloudinary = async (localFilePath)=>{
-    try {
-
-        console.log('Cloudinary Credentials:', process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_API_SECRET);
-
-        console.log(localFilePath);
+    try { 
         
         if(!localFilePath) return null;
         //uplaod the file in cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type : "auto"
-        })
-        //file has been uploaded successfully
-        console.log("response: " + response);
-        
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+        //file has been uploaded successfully   
         fs.unlinkSync(localFilePath);
         return response
-        
-        
+            
     } catch (error) {
-        console.log(error.message);
-        
+        console.log(error.message);  
         fs.unlinkSync(localFilePath) //remove the locally saved temporary file
         // as the upload operation got failed
-        console.log("download");
-        
-        return null;
-        
+        return null;    
     }
 
 }
